94301bec6e534865ab256acbc61950caef1a786c
Jack <xhzengAIB@gmail.com>
2014-01-23 15:20:12 +0000

    Add file with XHShareViewController class

---
+++ Project/XHCartoon/Controllers/XHShareViewController.m
@@ -0,0 +1,43 @@
+//
+//  XHShareViewController.m
+//  XHCartoon
+//
+//  Created by 曾 宪华 on 14-1-23.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHShareViewController.h"
+
+@interface XHShareViewController ()
+
+@end
+
+@implementation XHShareViewController
+
+#pragma mark - Life cycle
+
+- (void)_setup {
+    
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        [self _setup];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
