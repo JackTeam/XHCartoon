26e36f69fa00a8117408c23831d85a21d7b46bef
Jack <xhzengAIB@gmail.com>
2014-01-23 14:44:44 +0000

    Add file with XHCartoonCameraViewController class

---
+++ Project/XHCartoon/Controllers/XHCartoonCameraViewController.m
@@ -0,0 +1,43 @@
+//
+//  XHCartoonCameraViewController.m
+//  XHCartoon
+//
+//  Created by 曾 宪华 on 14-1-23.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHCartoonCameraViewController.h"
+
+@interface XHCartoonCameraViewController ()
+
+@end
+
+@implementation XHCartoonCameraViewController
+
+#pragma mark - Life cycle
+
+- (void)_setup {
+    
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        [self _setup];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
